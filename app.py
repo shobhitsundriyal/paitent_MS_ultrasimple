c9570e04be65c4088ecad9c7d4bc955306285b66
unknown <shobhitsundriyal0170@gmail.com>
2020-03-21 13:56:09 +0530

    some changes

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
 from firebase import firebase
 
 
-config = {
-    'apiKey': "AIzaSyBAziGjDRUBGJa9CB_R4iVo1_A9_EjHntU",
-    'authDomain': "tatkaal-e8a1b.firebaseapp.com",
-    'databaseURL': "https://tatkaal-e8a1b.firebaseio.com",
-    'projectId': "tatkaal-e8a1b",
-    'storageBucket': "tatkaal-e8a1b.appspot.com",
-    'messagingSenderId': "967129453853",
-    'appId': "1:967129453853:web:a2cb80b3d67dda25963df4",
-    'measurementId': "G-9FRVLGJKQG"
-  }
+
 
 firebase = firebase.FirebaseApplication('https://your_storage.firebaseio.com', None)
 result = firebase.get('/users', None)
